0afd14aa9a61c600ab92ff95761276901718aeeb
Ben Percy <thesvenskaanka@gmail.com>
2016-09-28 11:45:57 +1300

    just added a basic method

--- ToolPath.java
+++ ToolPath.java
@@ -118,7 +118,25 @@ public class ToolPath
     //save file with motor control values
     public void save_pwm_file(){
     //... this needs to be coded out 
-     
+             for ( int i = 0 ; i < pwm1_vector.size(); i++){
+            UI.println(pwm1_vector.get(i));
+        }
+        try {
+            //Whatever the file path is.
+            File statText = new File(fname);
+            FileOutputStream is = new FileOutputStream(statText);
+            OutputStreamWriter osw = new OutputStreamWriter(is);    
+            Writer w = new BufferedWriter(osw);
+            String str_out;
+            for (int i = 0; i <  pwm1_vector.size() ; i++){
+                str_out = String.format("%d,%d,%d\n",
+                    pwm1_vector.get(i), pwm2_vector.get(i),pwm1_vector.get(i));
+                w.write(str_out);
+            }
+            w.close();
+        } catch (IOException e) {
+            UI.println("Problem writing to the file statsTest.txt");
+        }
     }
 
 }
